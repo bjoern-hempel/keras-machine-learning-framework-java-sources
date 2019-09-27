cfe24da165a4a20193842a57710efa70443049ed
Björn Hempel <bjoern@hempel.li>
2019-09-28 00:51:58 +0200

    Update ImportNinePoints.java

--- src/main/java/ImportNinePoints.java
+++ src/main/java/ImportNinePoints.java
@@ -1,3 +1,34 @@
+/** 
+ * The ImportNinePoints class imports a keras model for a nine points example.
+ *
+ * @author Björn Hempel <bjoern@hempel.li>
+ * @version 1.0
+ * @web: https://github.com/bjoern-hempel/machine-learning-keras-suite
+ *
+ * LICENSE
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019 Björn Hempel <bjoern@hempel.li>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
 import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
